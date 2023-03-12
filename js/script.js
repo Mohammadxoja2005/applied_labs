aaecffc690eb51fbcf60e37413316029b8114322
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2023-03-12 19:41:33 +0500

    added swiper for you might also like section and configured slider to show

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// const swiper = new Swiper('.swiper-wrapper', {
+//     // Default parameters
+//     slidesPerView: 2,
+//     spaceBetween: 10,
+
+//     breakpoints: {
+//         // when window width is >= 320px
+//         320: {
+//             slidesPerView: 2,
+//             spaceBetween: 20
+//         },
+//         // when window width is >= 480px
+//         480: {
+//             slidesPerView: 3,
+//             spaceBetween: 30
+//         },
+//         // when window width is >= 640px
+//         640: {
+//             slidesPerView: 4,
+//             spaceBetween: 40
+//         }
+//     }
+// }) 
+
+// var swiper = new Swiper(".myswiper", {
+//     slidesPerView: 4,
+//     // spaceBetween: 16,
+//     // breakpoints: {
+//     //     // when window width is >= 320px
+//     //     320: {
+//     //         slidesPerView: 2,
+//     //         // spaceBetween: 20
+//     //     },
+//     //     // when window width is >= 480px
+//     //     480: {
+//     //         slidesPerView: 3,
+//     //         // spaceBetween: 30
+//     //     },
+//     //     // when window width is >= 640px
+//     //     640: {
+//     //         slidesPerView: 4,
+//     //         // spaceBetween: 40
+//     //     }
+//     // }
+// }); 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    //     breakpoints: {
+    //     // when window width is >= 768px
+    //     768: {
+    //         slidesPerView: "auto",
+    //         spaceBetween: 16
+    //     },
+
+    //     576: {
+    //         slidesPerView: "auto",
+    //         spaceBetween: 16
+    //     }
+    // }
+});
