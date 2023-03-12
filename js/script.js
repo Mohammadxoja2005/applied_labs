ee675c75b1bce1650d958e1efce7d3f31f955f8b
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2023-03-12 20:27:57 +0500

    added new hamburger and did display none when width of screen lower than 810

--- js/script.js
+++ js/script.js
@@ -47,16 +47,36 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 16,
-    //     breakpoints: {
-    //     // when window width is >= 768px
-    //     768: {
-    //         slidesPerView: "auto",
-    //         spaceBetween: 16
-    //     },
+    breakpoints: {
+        // when window width is >= 768px
+        768: {
+            slidesPerView: "auto",
+            spaceBetween: 16
+        },
 
-    //     576: {
-    //         slidesPerView: "auto",
-    //         spaceBetween: 16
-    //     }
-    // }
-});
+        576: {
+            slidesPerView: "auto",
+            spaceBetween: 8
+        },
+
+        1: {
+            slidesPerView: "auto",
+            spaceBetween: 8
+        }
+    }
+}); 
+
+
+// @media (min-width: 576px) { ... }
+
+// // Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) { ... }
+
+// // Large devices (desktops, 992px and up)
+// @media (min-width: 992px) { ... }
+
+// // X-Large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) { ... }
+
+// // XX-Large devices (larger desktops, 1400px and up)
+// @media (min-width: 1400px) { ... }
